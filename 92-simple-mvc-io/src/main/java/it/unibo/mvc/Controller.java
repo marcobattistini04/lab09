4cfd8fb728976225f2e497cf55b2e545d1367e90
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-18 11:54:00 +0100

    first attempt implementation of controller in folder 92-simple-mvc-io

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,41 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile;
+    private boolean setted = false;
+    private static final String PATH = System.getProperty("user.home") + File.separator;
+
+    public void setCurrentFile(File file) {
+        this.currentFile = file;
+        this.setted = true;
+    } 
+
+    public File getCurrentFile() {
+        File defensiveCopy = currentFile;
+        return defensiveCopy;
+    }
+
+    public String getCurrentFileName() {
+        if (!setted) {
+            return PATH + "output.txt";
+        }
+        return PATH + this.currentFile.getName();
+    }
+
+    public void writeInCurrentFile(String content) {
+        try (PrintStream ps = new PrintStream(getCurrentFileName(), StandardCharsets.UTF_8)){
+            ps.println(content);
+        } catch (IOException e) {
+            System.out.println("IO ERROR");
+        }
+    }
 
 }
