2f7b6e435b85ad146a0042eaa26188b1d1a0abd1
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-18 13:14:04 +0100

    first attempt implementation of simpleGUI

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,13 +1,69 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUI {
+    private final static int PROPORTION = 5;
+    private final JFrame frame = new JFrame("SimpleGUI");
 
-    private final JFrame frame = new JFrame();
+    public SimpleGUI(final Controller contr) {
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        final JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        mainPanel.add(textArea, BorderLayout.CENTER);
 
+        final JButton save = new JButton("Save");
+        mainPanel.add(save, BorderLayout.SOUTH);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int option = JOptionPane.showConfirmDialog(frame, "Do you really want to save?");
+                if(option == JOptionPane.YES_OPTION) {
+                    contr.writeInCurrentFile(textArea.getText());
+                }
+            }
+        });
+
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new SimpleGUI(new Controller()).display();
+    }
 }
