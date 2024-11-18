edcce64324d67f3a509569ea28d8aee6728dbad3
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-18 18:26:55 +0100

    first attempt implementation of SimpleGUIWithFileChooser

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -1,6 +1,23 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 
 
 /**
@@ -9,6 +26,94 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUIWithFileChooser {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("SimpleGUIWithFileChooser");
+    private static final int PROPORTION = 5;
 
+    /**
+     * Sets a simple GUI with file chooser. Has two buttons: 
+     * -Save: save the current text of the textArea in the selected file by the controller
+     * -Browse..: open the file chooser in order to choose the next selected file.
+     * The main panel il a border Layout and it contains a secondary Panel, that is always
+     * a Border Panel but located in the north section of main panel.
+     * @param contr
+     */
+    public SimpleGUIWithFileChooser(final Controller contr) {
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        final JTextArea textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        final JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        mainPanel.add(textArea, BorderLayout.CENTER);
+
+        final JButton save = new JButton("Save");
+        mainPanel.add(save, BorderLayout.SOUTH);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                int option = JOptionPane.showConfirmDialog(frame, "Do you really want to save?");
+                if (option == JOptionPane.YES_OPTION) {
+                    contr.writeInCurrentFile(textArea.getText());
+                }
+            }
+        });
+
+        final JPanel upperPanel = new JPanel();
+        upperPanel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField();
+        textField.setEditable(false);
+        upperPanel.add(textField, BorderLayout.CENTER);
+        final JButton browse = new JButton("Browse..");
+        upperPanel.add(browse, BorderLayout.AFTER_LINE_ENDS);
+        browse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                int option = fileChooser.showOpenDialog(browse.getParent());
+                if (option == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    contr.setCurrentFile(file);
+                    textField.setEditable(true);
+                    textField.setText(contr.getCurrentFileName());
+                    textField.setEditable(false);
+                }
+            }
+        });
+        mainPanel.add(upperPanel, BorderLayout.NORTH);
+        frame.setContentPane(mainPanel);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(final WindowEvent e) {
+                int n = JOptionPane.showConfirmDialog(frame, "Do you really want to quit?");
+                if (n == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
+    }
+
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application.
+     * @param args
+     */
+    public static void main(final String[] args) {
+        new SimpleGUIWithFileChooser(new Controller()).display();
+    }
 }
