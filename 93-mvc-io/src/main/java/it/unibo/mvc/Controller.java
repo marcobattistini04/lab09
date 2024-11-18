44045a2e720bf81fe8c7a20005b6d39da49c74bf
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-18 20:19:10 +0100

    created file .gitignore, .gitattributes; first attempt implementation of Controller in folder 93-MVC-IO

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * A simple controller that manage I/O access 
+ * and it is able to print on the standard output.
  */
 public interface Controller {
+    /**
+     * Sets the next string to be printed.
+     * @param str the next string to be printed
+     * @throws IllegalArgumentException if the string passed is null
+     */
+    public void setNextString(final String str);
+
+    /**
+     * @return the next string to be printed
+     */
+    public String getNextString();
+
+    /**
+     * @return the list of all printed strings
+     */
+    public List<String> getPrintedStringHistory();
+
+    /**
+     * Prints the current string.
+     * @throws IllegalStateException if it is not setted any current string
+     */
+    public void printCurrentString();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
- *
+ * class implementing the Controller interface 
  */
 public final class SimpleController implements Controller {
+    private final List<String> list = new ArrayList<>();
+    private String currentString;
+    @Override
+    public void setNextString(String str) {
+        if(str == null) {
+            throw new IllegalArgumentException("Cannot pass a null argument");
+        }
+        this.currentString = str;
+    }
+
+    @Override
+    public String getNextString() {
+        return this.currentString;
+    }
+
+    @Override
+    public List<String> getPrintedStringHistory() {
+        return this.list;
+    }
+
+    @Override
+    public void printCurrentString() {
+        if(this.currentString == null) {
+            throw new IllegalStateException("Cannot print anything beacuse the current string was not setted");
+        }
+        System.out.println(this.currentString); //NOPMD 
+        //in this case, the System.out is specificly requested
+        list.addFirst(this.currentString);
+    }
 
 }
