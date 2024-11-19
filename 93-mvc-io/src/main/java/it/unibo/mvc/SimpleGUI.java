627032c92d3ed5cbbd363e9a834648cdec84011c
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-11-19 10:13:55 +0100

    first attempt implementation of buttons "Print" and "ShowHistory"

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -2,15 +2,16 @@ package it.unibo.mvc;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import java.awt.BorderLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -31,25 +32,33 @@ public final class SimpleGUI {
         JButton print = new JButton("Print");
         JButton showHistory = new JButton("Show History");
 
+        print.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contr.setNextString(textField.getText());
+                contr.printCurrentString();
+            }
+        });
+
+        showHistory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<String> list = new ArrayList<>();
+                list = contr.getPrintedStringHistory();
+                for(String str: list) {
+                    textArea.append(str + "\n");
+                }
+            }
+        });
+
         mainPanel.add(textField, BorderLayout.NORTH);
         mainPanel.add(textArea, BorderLayout.CENTER);
         mainPanel.add(print, BorderLayout.SOUTH);
         mainPanel.add(showHistory, BorderLayout.SOUTH);
 
         frame.setContentPane(mainPanel);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(final WindowEvent e) {
-                final int n = JOptionPane.showConfirmDialog(frame, "Do you really want to quit?", "Quitting..", JOptionPane.YES_NO_OPTION);
-                if(n == JOptionPane.YES_OPTION) {
-                    System.exit(0);
-                }
-            }
-        });
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
-
 
     /**
      * Launches the application.
